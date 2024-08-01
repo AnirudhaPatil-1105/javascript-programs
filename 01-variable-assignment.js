edc4fb0c57c0889ff1f4c645a874c1cede7be6f9
AnirudhaPatil-1105 <panirudha18@gmail.com>
2024-08-01 22:52:10 +0530

    javascript variables
    javascript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("My City Name is: Jalgaon");
+console.log("My Bestfriend Name is: Saurav");
+console.log("My Country Name is: Bharat");
+console.log("My School Name is: Kashinath Palod Public School");
+console.log("My College Name is: MET Bhujbal Knowledge City");
---
+++ 01-variable.js
@@ -0,0 +1,15 @@
+//  Variable Declaration
+var firstName;
+console.log(firstName);
+
+// Variable Initialization
+firstName = "Janhavi";
+console.log(firstName);
+
+//  Variable Declaration and Variable Initialization
+var lastName = "Bagul";
+console.log(lastName) ;
+
+// Variable Update
+firstName = "Anirudha"
+console.log(firstName);
