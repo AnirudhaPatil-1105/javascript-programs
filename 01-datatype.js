c5b9deb3477b34d7e3703f237a57be5497fffdf8
AnirudhaPatil-1105 <panirudha18@gmail.com>
2024-08-02 14:21:55 +0530

    01-datatype.js
    01-datatype.js

---
+++ 01-datatype.js
@@ -0,0 +1,52 @@
+// Primitive Datatype Number, String and Boolean
+console.log("==================Number Datatype=================");
+var age =22;
+console.log("Age is:", age);
+var ageTypeOf = typeof age;
+console.log("datatype of age is:", typeof age);
+
+console.log("==================String Datatype=================");
+var city = 'Pune';
+console.log("City is:", city);
+var cityTypeOf = typeof city;
+console.log("type of city is:", cityTypeOf);
+
+var marks = "93"
+console.log("Marks is:", marks);
+var marksTypeOf = typeof marks;
+console.log("Type of marks is:", marksTypeOf);
+
+console.log("==================Boolean Datatype=================");
+var isMarried = true;
+console.log("isMarried :", isMarried);
+var isMarriedTypeOf = typeof isMarried;
+console.log("isMarried type of is:", isMarriedTypeOf);
+
+var isFailed = false;
+console.log("isFailed :", isFailed);
+var isFailedTypeOf = typeof isFailed;
+console.log("isFailed type of is:", isFailedTypeOf);
+
+console.log("==================Special Datatype=================");
+var country;
+console.log('Country value is:', country);
+var countryTypeOf = typeof country
+console.log("Type of country is:", countryTypeOf);
+
+console.log("==================Special Datatype: Null =================");
+var village = null;
+console.log("value of village is:", village);
+var villageTypeOf = typeof village;
+console.log("Type of village is:", villageTypeOf);
+
+
+
+
+
+
+
+
+
+
+
+
