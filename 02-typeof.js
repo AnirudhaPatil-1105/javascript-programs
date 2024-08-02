b6f3e088ab3b76ce6cdebb82eaa7cc3ffe75cdc0
AnirudhaPatil-1105 <panirudha18@gmail.com>
2024-08-02 15:04:18 +0530

    02-typeof.js
    02-typeof.js

---
+++ 02-typeof.js
@@ -0,0 +1,44 @@
+console.log("============================ 01 ============================");
+
+var bankname;
+console.log(" My Bank Name is:", bankname);
+
+bankname= "SBI Bank";
+console.log("My bank Name is:", bankname);
+
+var banknameTypeOf = "typeof bankname";
+console.log("My Bank Datatype is:", typeof bankname);
+console.log("============================ 02 ============================");
+
+var marks= '90%';
+console.log("My Marks are:", marks);
+
+var marksTypeOf = "typeof marks";
+console.log("My Marks Datatype is:", typeof marks);
+
+console.log("============================ 03 ============================");
+
+var isWorking;
+console.log("My Working Status is:", isWorking);
+
+isWorking= true;
+console.log("My Working Status is:", isWorking);
+var isWorkingTypeOf = "typeof isWorking";
+console.log("My Working Status Datatype is:", typeof isWorking);
+
+console.log("============================ 04 (a) ============================");
+
+var totalcount= "Hundred and Seven"
+console.log("Total Count is:", totalcount);
+
+var totalcountTypeOf = "typeof totalcount";
+console.log("Total Count Datatype is:", typeof totalcount);
+
+console.log("============================ 04 (b) ============================");
+var totalcount= 107;
+console.log("Total Count is:", totalcount);
+var totalcountTypeOf= "typeof totalcount";
+console.log("Total Count Datatype is:", typeof totalcount);
+
+
+
