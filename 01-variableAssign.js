770fe5381e5168e1344823c12f6f1cef318119de
AnirudhaPatil-1105 <panirudha18@gmail.com>
2024-08-02 14:21:00 +0530

    01-variableAssign.js
    01-variableAssign.js

---
+++ 01-variableAssign.js
@@ -0,0 +1,39 @@
+console.log("============================ 01 ============================");
+
+var myFullName;
+console.log("My Full Name Before Initialization:", myFullName);
+
+myFullName="Anirudha Patil";
+console.log("My First and Last Name After Initialization:", myFullName);
+
+myFullName="Anirudha Bharat Patil";
+console.log("My Full Name After Update:", myFullName);
+
+console.log("============================ 02 ============================");
+
+console.log("I Love only JS");
+
+console.log("============================ 03 ============================");
+myFullName="Anirudha";
+console.log("My First Name is:", myFullName);
+
+myFullName="Patil";
+console.log("My Last Name is:", myFullName);
+
+var age= 22;
+console.log("My Age is:", age);
+
+var address= "Karve Nagar, Pune"
+console.log("My Address is:", address);
+
+var pincode= 411052;
+console.log("My Area Pincode is:", pincode);
+
+var gender= "Male";
+console.log("My Gender is:", gender);
+
+var imMarried= false;
+console.log("I am Married:", imMarried);
+
+
+
