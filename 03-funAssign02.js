187d75b72e46ac412f410db3d6efefffc45fdda6
AnirudhaPatil-1105 <panirudha18@gmail.com>
2024-08-04 19:39:53 +0530

    03-funAssign02.js
    03-funAssign02.js

---
+++ 03-funAssign02.js
@@ -0,0 +1,10 @@
+function bankDetails(arg1 ,arg2, arg3, arg4){
+ 
+  console.log("My Bank Name is:",arg1," Account Number:",arg2," Location:",arg3," Pincode",arg4);
+  console.log("My Bank Name is:",arg1," Account Number:",arg2," Location:",arg3," Pincode",arg4);
+  console.log("My Bank Name is:",arg1," Account Number:",arg2," Location:",arg3," Pincode",arg4);
+  
+  }
+bankDetails(" CTI Bank"," 3456782345"," Pune"," 431202");
+bankDetails(" Axis Bank"," 7856723477"," Mumbai"," 441202");
+bankDetails(" HDFC Bank"," 8956782345"," Pune"," 631202",);
